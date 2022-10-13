4834e0d380ff86b24b8c6f38c25b75d9f30c8411
tony02stark <modichirag9999@gmail.com>
2022-10-14 00:24:11 +0530

    added new year counter

---
+++ projects/YearCounter/index.js
@@ -0,0 +1,37 @@
+let dayBox = document.getElementById("day-box");
+let hrBox = document.getElementById("hr-box");
+let minBox = document.getElementById("min-box");
+let secBox = document.getElementById("sec-box");
+let endDate = new Date(2022, 0, 1, 00, 00);
+let endTime = endDate.getTime();
+
+function countdown() {
+  let todayDate = new Date();
+  let todayTime = todayDate.getTime();
+  let remainingTime = endTime - todayTime;
+  let oneMin = 60 * 1000;
+  let oneHr = 60 * oneMin;
+  let oneDay = 24 * oneHr;
+
+  let addZeroes = (num) => (num < 10 ? `0${num}` : num);
+
+  if (endTime < todayTime) {
+    clearInterval(i);
+    document.querySelector(
+      ".countdown"
+    ).innerHTML = `<h1>Countdown Has Expired</h1>`;
+  } else {
+    let daysLeft = Math.floor(remainingTime / oneDay);
+    let hrsLeft = Math.floor((remainingTime % oneDay) / oneHr);
+    let minsLeft = Math.floor((remainingTime % oneHr) / oneMin);
+    let secsLeft = Math.floor((remainingTime % oneMin) / 1000);
+
+    dayBox.textContent = addZeroes(daysLeft);
+    hrBox.textContent = addZeroes(hrsLeft);
+    minBox.textContent = addZeroes(minsLeft);
+    secBox.textContent = addZeroes(secsLeft);
+  }
+}
+
+let i = setInterval(countdown, 1000);
+countdown();
