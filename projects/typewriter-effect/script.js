331833cb0c8fc3a38d2f7664a845aeda1af92c68
thegoodgamer14 <aryan.sgrbk@gmail.com>
2022-10-17 10:19:31 +0530

    added comments to script.js

--- projects/typewriter-effect/script.js
+++ projects/typewriter-effect/script.js
@@ -1,3 +1,4 @@
+// class for typing the details in <span data-words>
 class TypeWriter{
     constructor(txtElement, words, wait = 1500){
         this.txtElement = txtElement
@@ -9,30 +10,31 @@ class TypeWriter{
         this.isDeleting = false
     }
 
+    // function for typing the words
     type(){
-        const current = this.wordIndex % this.words.length
-        const fullTxt = this.words[current]
+        const current = this.wordIndex % this.words.length                  // current index of the word
+        const fullTxt = this.words[current]                                 // entire word to be typed out
 
-        if(this.isDeleting){
-            this.txt = fullTxt.substring(0, this.txt.length - 1)
+        if(this.isDeleting){                                                // check if the text is deleting 
+            this.txt = fullTxt.substring(0, this.txt.length - 1)            // remove the text
         }
         else{
-            this.txt = fullTxt.substring(0, this.txt.length + 1)
+            this.txt = fullTxt.substring(0, this.txt.length + 1)            // add the text
         }
         
-        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`  // insert text to the given element
         let typeSpeed = 500
         if(this.isDeleting){
             typeSpeed /= 2
         }
 
-        if(!this.isDeleting && this.txt === fullTxt){
-            typeSpeed = this.wait
-            this.isDeleting = true
+        if(!this.isDeleting && this.txt === fullTxt){                       // if the word has been fully typed
+            typeSpeed = this.wait                                           // wait for the designated time
+            this.isDeleting = true                                          // set isDeleting to true so the word can start deleting now 
         }
-        else if(this.isDeleting && this.txt === ''){
-            this.isDeleting = false
-            this.wordIndex++
+        else if(this.isDeleting && this.txt === ''){                        // if the word has been fully deleted
+            this.isDeleting = false                                         // set isDeleting to false so the word can start typing out now
+            this.wordIndex++                                                // move to the next word in array
             typeSpeed = 500
         }
 
